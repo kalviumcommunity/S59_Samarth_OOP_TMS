a132c5e4c8fc730d697171ed421d8a8e032f7e74
Samtav007 <samtav0821@gmail.com>
2024-08-18 14:40:12 +0530

    pointer

--- Task.cpp
+++ Task.cpp
@@ -4,22 +4,28 @@
 using namespace std;
 
 Task::Task(string t, string d, string dl)
-    : title(t), description(d), deadline(dl), isCompleted(false) {}
+    : title(t), description(d), deadline(dl), isCompleted(false) {
+    // Using `this` pointer to refer to the current object's members
+    this->title = t;
+    this->description = d;
+    this->deadline = dl;
+    this->isCompleted = false;
+}
 
 void Task::completeTask() {
-    isCompleted = true;
+    this->isCompleted = true;  // Using `this` pointer to modify the current object's member
 }
 
 void Task::displayTask() const {
-    cout << "Title: " << title << "\nDescription: " << description
-         << "\nDeadline: " << deadline << "\nStatus: " 
-         << (isCompleted ? "Completed" : "Incomplete") << endl;
+    cout << "Title: " << this->title << "\nDescription: " << this->description
+         << "\nDeadline: " << this->deadline << "\nStatus: " 
+         << (this->isCompleted ? "Completed" : "Incomplete") << endl;
 }
 
 string Task::getTitle() const {
-    return title;
+    return this->title;  // Using `this` pointer to return the current object's member
 }
 
 string Task::getDeadline() const {
-    return deadline;
+    return this->deadline;  // Using `this` pointer to return the current object's member
 }
