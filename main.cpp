913301066f9b127a58a8308e69b4177d88178a4f
Samtav007 <samtav0821@gmail.com>
2024-12-09 22:36:04 +0530

    done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "TMS.h"
+#include "TMS.H"
 #include "PriorityTask.h"
 #include "CategorizedTask.h"
 #include <iostream>
@@ -8,24 +8,27 @@ using namespace std;
 int main() {
     TMS* tms = new TMS();
 
+    
     Task* task1 = new PriorityTask("Finish Assignment", "Complete the math assignment", "2024-08-20", 1);
     Task* task2 = new CategorizedTask("Buy Groceries", "Buy vegetables and fruits", "2024-08-17", "Shopping");
     Task* task3 = new Task("Workout", "Do 30 minutes of exercise", "2024-08-18");
 
+    
     tms->addTask(task1);
-    tms->addTask(task2);
-    tms->addTask(task3);
+    vector<Task*> additionalTasks = { task2, task3 };
+    tms->addTask(additionalTasks);  
 
+    
     cout << "Tasks before sorting:" << endl;
     tms->viewTasks();
 
+    
     tms->sortTasksByDeadline();
-
     cout << "Tasks after sorting by deadline:" << endl;
     tms->viewTasks();
 
+    
     tms->completeTaskByTitle("Workout");
-
     cout << "Tasks after completing 'Workout':" << endl;
     tms->viewTasks();
 
